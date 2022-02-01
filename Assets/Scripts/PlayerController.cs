560d0653b407dda2e5b9c64da5028d03c231e937
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-02-01 18:30:04 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float horizontalMovement;
+    float verticalMovement;
+
+    float cameraPitch;
+
+    float speedModifer;
+    float lookSensitivity;
+
+    public Transform playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // init
+        speedModifer = 4f;
+        lookSensitivity = 5f;
+        cameraPitch = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+        PlayerLook();
+    }
+
+    // -------------------------------------------------------------- WASD Movement
+    void PlayerMove()
+    {     
+        horizontalMovement = Input.GetAxis("Horizontal") * speedModifer * Time.deltaTime;
+        transform.Translate(horizontalMovement, 0, 0);
+
+        verticalMovement = Input.GetAxis("Vertical") * speedModifer * Time.deltaTime;
+        transform.Translate(0, 0, verticalMovement);
+    }
+
+    // ------------------------------------------------------------- Mouse Look
+    void PlayerLook()
+    {
+        Vector2 mouseLook = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * lookSensitivity);
+
+        cameraPitch -= mouseLook.y * lookSensitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90, 90);
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        transform.Rotate(Vector3.up * mouseLook.x);
+    }
+}
