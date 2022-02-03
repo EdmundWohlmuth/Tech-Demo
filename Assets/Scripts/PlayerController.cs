9cad1cef1f50647015afd5d753c735eaf0483ae2
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-02-03 18:20:14 -0400

    refactored code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,60 +6,118 @@ public class PlayerController : MonoBehaviour
 {
     float horizontalMovement;
     float verticalMovement;
+    Vector3 characterMovement;
+
     float gravity;
-    float gravityVelocity;
+    float jumpHeight;
 
     float cameraPitch;
 
     float speedModifer;
     float lookSensitivity;
 
+    bool isPaused = false;
+
+    float velocityY;
+
     public Transform playerCamera;
-    CharacterController controller = null;
+    public CharacterController controller;
 
     // Start is called before the first frame update
     void Start()
     {
         // init
         speedModifer = 4f;
-        lookSensitivity = 5f;
+        lookSensitivity = 10f;
         cameraPitch = 0.0f;
-        gravity = 9.81f;
-        gravityVelocity = 0.0f;
+        gravity = -9.81f;
+        jumpHeight = 5.0f;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        PlayerMove();
-        PlayerLook();
+    {       
+        Move();
+        Look();
+        Jump();
+        Falling();
+        Paused();
+
+
     }
 
-    // -------------------------------------------------------------- WASD Movement
-    void PlayerMove()
-    {     
+    // -------------------------------------------------------------- Movement
+    void Move()
+    {          
         horizontalMovement = Input.GetAxis("Horizontal") * speedModifer * Time.deltaTime;
         transform.Translate(horizontalMovement, 0, 0);
 
         verticalMovement = Input.GetAxis("Vertical") * speedModifer * Time.deltaTime;
         transform.Translate(0, 0, verticalMovement);
 
-        if (controller.isGrounded)
-        {
-            gravityVelocity = 0.0f;
-        }
-        gravityVelocity += gravity * Time.deltaTime;
+        characterMovement = new Vector3(horizontalMovement, velocityY, verticalMovement);
+      
     }
 
     // ------------------------------------------------------------- Mouse Look
-    void PlayerLook()
+    void Look()
     {
         Vector2 mouseLook = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * lookSensitivity);
 
-        cameraPitch -= mouseLook.y * lookSensitivity; // 
+        cameraPitch -= mouseLook.y; 
         cameraPitch = Mathf.Clamp(cameraPitch, -90, 90);
         playerCamera.localEulerAngles = Vector3.right * cameraPitch;
 
         transform.Rotate(Vector3.up * mouseLook.x);
+    }
+
+    // -------------------------------------------------------------- Jumping
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            velocityY += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+    }
+
+    // ---------- I'll figure this out later --------------------- isGrounded
+    void Falling()
+    {
+        if (controller.isGrounded && velocityY < 0)
+        {
+            Debug.Log("is grounded");
+            velocityY = 0;
+        }
+        else
+        {
+            Debug.Log("isn't grounded");
+
+        }
+    }
+
+    // *might move* ---------------------------------------------- Pause functions
+    void Paused()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                isPaused = true;
+            }
+
+            isPaused = false;
+        }
+
+        if (!isPaused)
+        {
+            // Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        if (isPaused)
+        {
+            // Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 }
