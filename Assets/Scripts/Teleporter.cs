30994d86fe19fe0faf8b38e960df45d7e1fa5d9e
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-03-11 12:25:08 -0400

    Added Teleporter Sounds

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -6,11 +6,14 @@ public class Teleporter : MonoBehaviour
 {
     public CharacterController player;
     public GameObject TelePoint;
+    public AudioClip teleSound;
+    public AudioSource teleportSource;
 
     private void OnTriggerEnter(Collider other)
     {
         player.enabled = false;
         player.transform.position = TelePoint.transform.position;
+        teleportSource.PlayOneShot(teleSound);
         player.enabled = true;
     }
 }
